49a6ca92159899049eacafbc9652a510e8c9049e
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-03-02 14:25:22 +0100

    Add project files.

---
+++ MvcCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        private HelperPathProvider helperPath;
+        public UploadFilesController(HelperPathProvider helperPath)
+        {
+            this.helperPath = helperPath;
+        }
+
+        public IActionResult SubirFichero()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubirFichero(IFormFile fichero)
+        {
+            string filename = fichero.FileName;
+            string path = this.helperPath.MapFolder(filename, Folder.Uploads);
+            using(Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await fichero.CopyToAsync(stream);
+            }
+            ViewData["MENSAJE"] = "Fichero subido a " + path;
+            return View();
+        }
+    }
+}
