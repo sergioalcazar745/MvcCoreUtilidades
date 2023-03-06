c99c5c48b35e36563af0a5d2518069b325a613a3
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-03-06 14:27:16 +0100

    06/03/2023

---
+++ MvcCoreUtilidades/Controllers/CifradosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class CifradosController : Controller
+    {
+        public IActionResult CifradoBasico()
+        {
+            string resultado = HelperCryptography.EncriptarTextoBasico("a12345");
+            ViewData["TEXTOCIFRADO"] = resultado;
+            return View();
+        }
+    }
+}
---
+++ MvcCoreUtilidades/Helpers/HelperCryptography.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MvcCoreUtilidades.Helpers
+{
+    public class HelperCryptography
+    {
+        public static string EncriptarTextoBasico(string contenido)
+        {
+            byte[] entrada;
+            byte[] salida;
+
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            SHA1Managed sha1 = new SHA1Managed();
+
+            entrada = encoding.GetBytes(contenido);
+            salida = sha1.ComputeHash(entrada);
+            string resultado = encoding.GetString(salida);
+
+            return resultado;
+        }
+    }
+}
