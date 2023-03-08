cde9a6d899320e5c5f8ebae9b2d0cec9645dc25e
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-03-08 10:50:11 +0100

    CIFRADOS

--- MvcCoreUtilidades/Controllers/CifradosController.cs
+++ MvcCoreUtilidades/Controllers/CifradosController.cs
@@ -5,10 +5,59 @@ namespace MvcCoreUtilidades.Controllers
 {
     public class CifradosController : Controller
     {
+        public IActionResult CifradoEficiente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CifradoEficiente(string contenido, string resultado, string accion)
+        {
+            if (accion.ToLower() == "cifrar") {
+                string response = HelperCryptography.EncriptarContenido(contenido, false);
+                ViewData["TEXTOCIFRADO"] = response;
+            }
+            else if (accion.ToLower() == "comparar")
+            {
+                string response = HelperCryptography.EncriptarContenido(contenido, true);
+                if (response != resultado)
+                {
+                    ViewData["MENSAJE"] = "<h1 style='color: blue'>Son distintos</h1>";
+                }
+                else
+                {
+                    ViewData["MENSAJE"] = "<h1 style='color: blue'>Son iguales</h1>";
+                }
+            }
+            return View();
+        }
+
+
         public IActionResult CifradoBasico()
         {
-            string resultado = HelperCryptography.EncriptarTextoBasico("a12345");
-            ViewData["TEXTOCIFRADO"] = resultado;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CifradoBasico(string contenido, string resultado, string accion)
+        {
+            string response = HelperCryptography.EncriptarTextoBasico(contenido);
+            if(accion.ToLower() == "cifrar")
+            {
+                ViewData["TEXTOCIFRADO"] = response;
+            }
+            else if (accion.ToLower() == "comparar")
+            {
+                if(response != resultado)
+                {
+                    ViewData["MENSAJE"] = "Los valores no coinciden";
+                }
+                else
+                {
+                    ViewData["MENSAJE"] = "Contenidos iguales!!!";
+
+                }
+            }
             return View();
         }
     }
--- MvcCoreUtilidades/Helpers/HelperCryptography.cs
+++ MvcCoreUtilidades/Helpers/HelperCryptography.cs
@@ -5,6 +5,41 @@ namespace MvcCoreUtilidades.Helpers
 {
     public class HelperCryptography
     {
+        public static string Salt { get; set; }
+
+        private static string GenerateSalt()
+        {
+            Random random = new Random();
+            string salt = "";
+            for(int i = 0; i <= 50; i++)
+            {
+                int alet = random.Next(0, 255);
+                char letra = Convert.ToChar(alet);
+                salt += letra;
+            }
+            return salt;
+        }
+
+        public static string EncriptarContenido(string contenido, bool comparar)
+        {
+            if(comparar == false)
+            {
+                Salt = GenerateSalt();
+            }
+            string contenidoSalt = contenido + Salt;
+            SHA256 sHA256 = SHA256.Create();
+            byte[] salida;
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            salida = encoding.GetBytes(contenidoSalt);
+            for(int i = 1; i <= 24; i++)
+            {
+                salida = sHA256.ComputeHash(salida);
+            }
+            sHA256.Clear();
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+
         public static string EncriptarTextoBasico(string contenido)
         {
             byte[] entrada;
